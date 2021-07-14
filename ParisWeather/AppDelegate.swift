90a125216ec2a3d1cb72a8b40144fb6b783e88ea
reda.mimouni <reda.mimouni-ext@malakoffhumanis.com>
2021-07-14 09:16:38 +0200

    Init project

---
+++ ParisWeather/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ParisWeather
+//
+//  Created by reda.mimouni on 14/07/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
